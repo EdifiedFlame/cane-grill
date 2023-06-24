9a115a2c6b1e74a9f4cebc0acf06f37545ae3048
EdifiedFlame <thalesssacilotti@outlook.com>
2023-06-23 22:21:07 -0300

    cannes grill

---
+++ usuarios/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def cadastro(request):
+     pass
+
+def login(request):
+     return render(request, 'login.html')
+
+def dashboard(request):
+    pass
+
+def logout(request):
+   pass
